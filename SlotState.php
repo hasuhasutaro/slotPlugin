6afaa0714fcd91e02f6d13970e4bb311b8308efa
noahasu <64357548+hasuhasutaro@users.noreply.github.com>
2023-07-24 09:21:16 +0900

    Delete SlotState.php

--- SlotState.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace noahasu\slot\enum;
-enum SlotState {
-    case NORMAL; // 通常。777でジャックポットチャンス, 1/10000でフィーバー
-    case FEVER; // フィーバー。777でジャックポットチャンス
-    case JACKPOT_CHANCE; // ぞろ目でジャックポットへ
-    case ULTRA_JACKPOT;
-}
